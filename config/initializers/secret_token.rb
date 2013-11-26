321e834598ec2b8a0a0c0b3d025cb099a27c53ad
mytv <tranmy92@gmail.com>
2013-11-26 04:38:12 -0500

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+App123::Application.config.secret_key_base = 'dca62f187e501b00fb228e62baae9aef273f98427283010888b600f30f1a8be93f88e4b73e02e81b53ebf769c973531122c8ae792aefeb86d7c666ab3ba42363'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+App123::Application.config.session_store :cookie_store, key: '_app123_session'
